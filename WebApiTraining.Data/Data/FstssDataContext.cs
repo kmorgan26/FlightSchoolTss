f0ef6a915bffe472761aac87dd03d69868bb1f12
Kevin Morgan <kmorgan26@gmail.com>
2023-04-29 09:27:08 -0500

    add database contexts (data and identity)

---
+++ WebApiTraining.Data/Data/FstssDataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiTraining.Data.Entities;
+
+namespace WebApiTraining.Data.Data
+{
+    public class FstssDataContext : DbContext
+    {
+        public FstssDataContext(DbContextOptions<FstssDataContext> options) : base(options)
+        {
+        }
+        public DbSet<Maintainer> Maintainers { get; set; }
+        public DbSet<Platform> Platforms { get; set; }
+        public DbSet<Simulator> Simulators { get; set; }
+    }
+}
