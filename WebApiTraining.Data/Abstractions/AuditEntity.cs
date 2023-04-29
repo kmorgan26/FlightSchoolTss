32da3271e3febb7e7ed667a2a0fe5ce2cea1b93a
Kevin Morgan <kmorgan26@gmail.com>
2023-04-29 09:26:46 -0500

    Add abstract classes for entities

---
+++ WebApiTraining.Data/Abstractions/AuditEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiTraining.Data.Abstractions;
+
+public abstract class AuditEntity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    public string CreatedBy { get; set; }
+    public DateTime? LastModifiedDate { get; set; }
+    public string LastModifiedBy { get; set; }
+}
---
+++ WebApiTraining.Data/Abstractions/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiTraining.Data.Abstractions
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
