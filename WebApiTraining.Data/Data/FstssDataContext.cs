5d3e06f413d2c04fe88954642945e9888b655e07
Kevin Morgan <kmorgan26@gmail.com>
2023-04-30 11:08:16 -0500

    Add functionality to create seed data

--- WebApiTraining.Data/Data/FstssDataContext.cs
+++ WebApiTraining.Data/Data/FstssDataContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebApiTraining.Data.Configurations;
 using WebApiTraining.Data.Entities;
 
 namespace WebApiTraining.Data.Data
@@ -8,6 +9,16 @@ namespace WebApiTraining.Data.Data
         public FstssDataContext(DbContextOptions<FstssDataContext> options) : base(options)
         {
         }
+
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new MaintainerConfiguration());
+            modelBuilder.ApplyConfiguration(new PlatformConfiguration());
+            modelBuilder.ApplyConfiguration(new SimulatorConfiguration());
+        }
+
         public DbSet<Maintainer> Maintainers { get; set; }
         public DbSet<Platform> Platforms { get; set; }
         public DbSet<Simulator> Simulators { get; set; }
