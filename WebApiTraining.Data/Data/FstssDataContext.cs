aa65406d3156e92a3317dc3b8e48505f894b458a
Kevin Morgan <kmorgan26@gmail.com>
2023-04-30 14:59:18 -0500

    Add design-time factory

--- WebApiTraining.Data/Data/FstssDataContext.cs
+++ WebApiTraining.Data/Data/FstssDataContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 using WebApiTraining.Data.Configurations;
 using WebApiTraining.Data.Entities;
 
@@ -22,5 +24,26 @@ namespace WebApiTraining.Data.Data
         public DbSet<Maintainer> Maintainers { get; set; }
         public DbSet<Platform> Platforms { get; set; }
         public DbSet<Simulator> Simulators { get; set; }
+    }
+    public class FstssDataContextFactory : IDesignTimeDbContextFactory<FstssDataContext>
+    {
+        public FstssDataContext CreateDbContext(string[] args)
+        {
+            //get environment
+            string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            //build config
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile("secrets.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            // get conn string
+            var optionsBuilder = new DbContextOptionsBuilder<FstssDataContext>();
+            var connectionString = config.GetConnectionString("FstssDataConnectionString");
+            optionsBuilder.UseSqlServer(connectionString);
+            return new FstssDataContext(optionsBuilder.Options);
+        }
     }
 }
