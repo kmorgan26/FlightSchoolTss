d300645c7c2fe16f4a6768d1853b4c12d3e3196c
Kevin D. Morgan <kevin.d.morgan@gdit.com>
2023-05-01 14:57:39 -0500

    Add Maintainer DTOs to Mapper Config

---
+++ WebApiTraining/Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using WebApiTraining.Data.Entities;
+using WebApiTraining.DTOs.Maintainer;
+
+namespace WebApiTraining.Configurations;
+
+public class MapperConfig : Profile
+{
+    public MapperConfig()
+    {
+        CreateMap<Maintainer, MaintainerDto>().ReverseMap();
+        CreateMap<Maintainer, CreateMaintainerDto>().ReverseMap();
+    }
+}
