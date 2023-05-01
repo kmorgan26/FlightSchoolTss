b252630d7178b1668eb26668fa224b794bd75861
Kevin D. Morgan <kevin.d.morgan@gdit.com>
2023-05-01 15:55:15 -0500

    Add SimulatorDTOs

---
+++ WebApiTraining/DTOs/Simulator/CreateSimulatorDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiTraining.DTOs.Simulator;
+
+public class CreateSimulatorDto
+{
+    public string Name { get; set; }
+    public string Alias { get; set; }
+    public int PlatformId { get; set; }
+    public bool IsActive { get; set; }
+}
---
+++ WebApiTraining/DTOs/Simulator/SimulatorDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiTraining.DTOs.Simulator;
+
+public class SimulatorDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Alias { get; set; }
+    public int PlatformId { get; set; }
+    public bool IsActive { get; set; }
+}
