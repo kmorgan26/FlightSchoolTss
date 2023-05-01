959b9292aaf3fefdbe99bd8edd53c2f8fc3ca512
Kevin D. Morgan <kevin.d.morgan@gdit.com>
2023-05-01 15:35:05 -0500

    Modify Platform Endpoints to use DTOs

--- WebApiTraining/Endpoints/PlatformEndpoints.cs
+++ WebApiTraining/Endpoints/PlatformEndpoints.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AutoMapper;
 using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.OpenApi;
+using Microsoft.EntityFrameworkCore;
 using WebApiTraining.Data.Data;
 using WebApiTraining.Data.Entities;
+using WebApiTraining.DTOs.Platform;
 
 namespace WebApiTraining.Endpoints;
 
@@ -12,32 +13,35 @@ public static class PlatformEndpoints
     {
         var group = routes.MapGroup("/api/Platform").WithTags(nameof(Platform));
 
-        group.MapGet("/", async (FstssDataContext db) =>
+        group.MapGet("/", async (FstssDataContext db, IMapper mapper) =>
         {
-            return await db.Platforms.ToListAsync();
+            var platforms = await db.Platforms.ToListAsync();
+            return mapper.Map<List<PlatformDto>>(platforms);
         })
         .WithName("GetAllPlatforms")
         .WithOpenApi();
 
-        group.MapGet("/{id}", async Task<Results<Ok<Platform>, NotFound>> (int id, FstssDataContext db) =>
+        group.MapGet("/{id}", async Task<Results<Ok<PlatformDto>, NotFound>> (int id, FstssDataContext db, IMapper mapper) =>
         {
-            return await db.Platforms.AsNoTracking()
-                .FirstOrDefaultAsync(model => model.Id == id)
-                is Platform model
-                    ? TypedResults.Ok(model)
-                    : TypedResults.NotFound();
+            var platform = await db.Platforms.AsNoTracking()
+                .FirstOrDefaultAsync(model => model.Id == id);
+
+            var result = mapper.Map<PlatformDto>(platform);
+
+            return platform is null ? TypedResults.NotFound() : TypedResults.Ok(result);
+                
         })
         .WithName("GetPlatformById")
         .WithOpenApi();
 
-        group.MapPut("/{id}", async Task<Results<Ok, NotFound>> (int id, Platform platform, FstssDataContext db) =>
+        group.MapPut("/{id}", async Task<Results<Ok, NotFound>> (int id, PlatformDto platformDto, FstssDataContext db, IMapper mapper) =>
         {
             var affected = await db.Platforms
                 .Where(model => model.Id == id)
                 .ExecuteUpdateAsync(setters => setters
-                  .SetProperty(m => m.IsActive, platform.IsActive)
-                  .SetProperty(m => m.Id, platform.Id)
-                  .SetProperty(m => m.Name, platform.Name)
+                  .SetProperty(m => m.IsActive, platformDto.IsActive)
+                  .SetProperty(m => m.Name, platformDto.Name)
+                  .SetProperty(m => m.MaintainerId, platformDto.MaintainerId)
                 );
 
             return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
@@ -45,11 +49,15 @@ public static class PlatformEndpoints
         .WithName("UpdatePlatform")
         .WithOpenApi();
 
-        group.MapPost("/", async (Platform platform, FstssDataContext db) =>
+        group.MapPost("/", async (CreatePlatformDto createPlatformDto, FstssDataContext db, IMapper mapper) =>
         {
+            var platform = mapper.Map<Platform>(createPlatformDto);
             db.Platforms.Add(platform);
+            
             await db.SaveChangesAsync();
-            return TypedResults.Created($"/api/Platform/{platform.Id}", platform);
+
+            var result = mapper.Map<PlatformDto>(platform);
+            return TypedResults.Created($"/api/Platform/{platform.Id}", result);
         })
         .WithName("CreatePlatform")
         .WithOpenApi();
