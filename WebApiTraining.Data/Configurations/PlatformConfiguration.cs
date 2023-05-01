b9b3b487b5014036d5b4f6448dda530c1e64d601
Kevin D. Morgan <kevin.d.morgan@gdit.com>
2023-05-01 13:36:50 -0500

    Update PlatformConfig and add Lot Config

--- WebApiTraining.Data/Configurations/PlatformConfiguration.cs
+++ WebApiTraining.Data/Configurations/PlatformConfiguration.cs
@@ -15,7 +15,8 @@ internal class PlatformConfiguration : IEntityTypeConfiguration<Platform>
                 new Platform { Id=4, Name = "UH-60L", MaintainerId = 2, IsActive = true },
                 new Platform { Id=5, Name = "UH-60M", MaintainerId = 2, IsActive = true },
                 new Platform { Id=6, Name = "UH-72A", MaintainerId = 1, IsActive = true },
-                new Platform { Id=7, Name = "UH-72CPT", MaintainerId = 1, IsActive = true }
+                new Platform { Id=7, Name = "UH-72CPT", MaintainerId = 1, IsActive = true },
+                new Platform { Id=8, Name ="RCTD", MaintainerId=2, IsActive=true }
             );
     }
 }
