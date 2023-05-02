b768f83ee796122b5f6129a6ce1386c7384cd696
Kevin Morgan <kmorgan26@gmail.com>
2023-05-02 04:28:21 -0500

    commented FstssDataContextFactory

--- WebApiTraining.Data/Data/FstssDataContext.cs
+++ WebApiTraining.Data/Data/FstssDataContext.cs
@@ -28,25 +28,25 @@ namespace WebApiTraining.Data.Data
         public DbSet<Lot> Lots { get; set; }
 
     }
-    public class FstssDataContextFactory : IDesignTimeDbContextFactory<FstssDataContext>
-    {
-        public FstssDataContext CreateDbContext(string[] args)
-        {
-            //get environment
-            string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+    //public class FstssDataContextFactory : IDesignTimeDbContextFactory<FstssDataContext>
+    //{
+    //    public FstssDataContext CreateDbContext(string[] args)
+    //    {
+    //        //get environment
+    //        string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
-            //build config
-            IConfiguration config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile("secrets.json", optional: true, reloadOnChange: true)
-                .Build();
+    //        //build config
+    //        IConfiguration config = new ConfigurationBuilder()
+    //            .SetBasePath(Directory.GetCurrentDirectory())
+    //            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    //            .AddJsonFile("secrets.json", optional: true, reloadOnChange: true)
+    //            .Build();
 
-            // get conn string
-            var optionsBuilder = new DbContextOptionsBuilder<FstssDataContext>();
-            var connectionString = config.GetConnectionString("FstssDataConnectionString");
-            optionsBuilder.UseSqlServer(connectionString);
-            return new FstssDataContext(optionsBuilder.Options);
-        }
-    }
+    //        // get conn string
+    //        var optionsBuilder = new DbContextOptionsBuilder<FstssDataContext>();
+    //        var connectionString = config.GetConnectionString("FstssDataConnectionString");
+    //        optionsBuilder.UseSqlServer(connectionString);
+    //        return new FstssDataContext(optionsBuilder.Options);
+    //    }
+    //}
 }
