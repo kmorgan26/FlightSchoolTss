c5e5058d9ff146d38b7d91bc9ae19a140e9915d1
Kevin Morgan <kmorgan26@gmail.com>
2023-05-05 11:41:49 -0500

    Create DTO for lot

---
+++ WebApiTraining/DTOs/Lot/CreateLotDto.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApiTraining.DTOs.Lot;
+
+public class CreateLotDto
+{
+    public string Name { get; set; }
+    public int PlatformId { get; set; }
+}
---
+++ WebApiTraining/DTOs/Lot/LotDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApiTraining.DTOs.Lot;
+
+public class LotDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int PlatformId { get; set; }
+}
