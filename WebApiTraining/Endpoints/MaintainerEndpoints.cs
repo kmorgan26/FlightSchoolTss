dbf7b3609a98e5037989d0f22686e534addd613a
Kevin Morgan <kmorgan26@gmail.com>
2023-05-06 10:10:35 -0500

    Refactored endpoints to use repo

--- WebApiTraining/Endpoints/MaintainerEndpoints.cs
+++ WebApiTraining/Endpoints/MaintainerEndpoints.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 using WebApiTraining.Data.Data;
 using WebApiTraining.Data.Entities;
+using WebApiTraining.Data.Interfaces;
 using WebApiTraining.DTOs.Maintainer;
 
 namespace WebApiTraining.Endpoints;
@@ -13,18 +14,17 @@ public static class MaintainerEndpoints
     {
         var group = routes.MapGroup("/api/maintainer").WithTags(nameof(Maintainer));
 
-        group.MapGet("/", async (FstssDataContext db, IMapper mapper) =>
+        group.MapGet("/", async (IMaintainerRepository repo, IMapper mapper) =>
         {
-            var maintainers = await db.Maintainers.ToListAsync();
+            var maintainers = await repo.GetAllAsync();
             return mapper.Map<List<MaintainerDto>>(maintainers);
         })
         .WithName("GetAllMaintainers")
         .WithOpenApi();
 
-        group.MapGet("/{id}", async Task<Results<Ok<MaintainerDto>, NotFound>> (int id, FstssDataContext db, IMapper mapper) =>
+        group.MapGet("/{id}", async Task<Results<Ok<MaintainerDto>, NotFound>> (int id, IMaintainerRepository repo, IMapper mapper) =>
         {
-            var maintainer = await db.Maintainers.AsNoTracking()
-                .FirstOrDefaultAsync(model => model.Id == id);
+            var maintainer = await repo.GetAsync(id);
 
             if (maintainer == null) return TypedResults.NotFound();
 
@@ -36,41 +36,37 @@ public static class MaintainerEndpoints
         .WithName("GetMaintainerById")
         .WithOpenApi();
 
-        group.MapPut("/{id}", async Task<Results<Ok, NotFound>> (int id, MaintainerDto maintainerDto, FstssDataContext db) =>
+        group.MapPut("/{id}", async (int id, MaintainerDto maintainerDto, IMaintainerRepository repo, IMapper mapper) =>
         {
-            var affected = await db.Maintainers
-                .Where(model => model.Id == id)
-                .ExecuteUpdateAsync(setters => setters
-                  .SetProperty(m => m.Name, maintainerDto.Name)
-                );
+            var affected = await repo.GetAsync(id);
 
-            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+            if (affected is null) 
+                return Results.NotFound();
+            
+            mapper.Map(maintainerDto, affected);
+            await repo.UpdateAsync(maintainerDto.Id);
+
+            return Results.NoContent();
         })
         .WithName("UpdateMaintainer")
         .WithOpenApi();
 
-        group.MapPost("/", async (CreateMaintainerDto maintainerDto, FstssDataContext db, IMapper mapper) =>
+        group.MapPost("/", async (CreateMaintainerDto maintainerDto, IMaintainerRepository repo, IMapper mapper) =>
         {
             var maintainer = mapper.Map<Maintainer>(maintainerDto);
-            db.Maintainers.Add(maintainer);
-            
-            await db.SaveChangesAsync();
-
-            var result = mapper.Map<MaintainerDto>(maintainer);
-            return TypedResults.Created($"/api/maintainer/{maintainer.Id}", result);
+            await repo.AddAsync(maintainer);
+            return TypedResults.Created($"/api/maintainer/{maintainer.Id}", maintainerDto);
         })
         .WithName("CreateMaintainer")
         .WithOpenApi();
 
-        group.MapDelete("/{id}", async Task<Results<Ok, NotFound>> (int id, FstssDataContext db) =>
+        group.MapDelete("/{id}", async (int id, IMaintainerRepository repo) =>
         {
-            var affected = await db.Maintainers
-                .Where(model => model.Id == id)
-                .ExecuteDeleteAsync();
-
-            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+            return await repo.DeleteAsync(id) ? Results.NoContent() : Results.NotFound();
         })
+        .WithTags(nameof(Maintainer))
         .WithName("DeleteMaintainer")
-        .WithOpenApi();
+        .Produces<Maintainer>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status404NotFound);
     }
 }
