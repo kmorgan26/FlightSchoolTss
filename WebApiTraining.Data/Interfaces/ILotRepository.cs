f467ed46a2de5e58ad0952a29016e706e9914e8d
Kevin Morgan <kmorgan26@gmail.com>
2023-05-06 10:58:38 -0500

    Create Lot Repo

---
+++ WebApiTraining.Data/Interfaces/ILotRepository.cs
@@ -0,0 +1,4 @@
+﻿using WebApiTraining.Data.Entities;
+
+namespace WebApiTraining.Data.Interfaces;
+public interface ILotRepository : IGenericRepository<Lot>{}
---
+++ WebApiTraining.Data/Repositories/LotRepository.cs
@@ -0,0 +1,11 @@
+﻿using WebApiTraining.Data.Data;
+using WebApiTraining.Data.Entities;
+using WebApiTraining.Data.Interfaces;
+
+namespace WebApiTraining.Data.Repositories;
+public class LotRepository : GenericRepository<Lot>, ILotRepository
+{
+    public LotRepository(FstssDataContext context) : base(context)
+    {
+    }
+}
