72ab218b93eba95138c29adfdae03c52c5eb5ba7
Kevin Morgan <kmorgan26@gmail.com>
2023-05-09 09:34:08 -0500

    Add First and Last Name to extended Identity User class, FstssUser

---
+++ WebApiTraining.Data/FstssUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApiTraining.Data;
+public class FstssUser : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+}
