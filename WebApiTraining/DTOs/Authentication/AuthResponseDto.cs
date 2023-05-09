418079bfe2a18335be6dc36fdba150e35c95a7a2
Kevin Morgan <kmorgan26@gmail.com>
2023-05-09 13:27:50 -0500

    Add DTOs to support Login and Authentication Response

---
+++ WebApiTraining/DTOs/Authentication/AuthResponseDto.cs
@@ -0,0 +1,6 @@
+﻿namespace WebApiTraining.DTOs.Authentication;
+public class AuthResponseDto
+{
+    public string UserId { get; set; }
+    public string Token { get; set; }
+}
---
+++ WebApiTraining/DTOs/Authentication/LoginDto.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApiTraining.DTOs.Authentication;
+
+public class LoginDto
+{
+    public string UserName { get; set; } = null!;
+    public string Password { get; set; } = null!;
+}
