d0d03a2c0314b38f48ca7a4d6b503bb70d0a7ab9
Kevin Morgan <kmorgan26@gmail.com>
2023-05-09 13:28:23 -0500

    Add Endpoints for Auth

---
+++ WebApiTraining/Endpoints/AuthenticationEndpoints.cs
@@ -0,0 +1,27 @@
+﻿using WebApiTraining.DTOs.Authentication;
+using WebApiTraining.Services;
+
+namespace WebApiTraining.Endpoints;
+
+public static class AuthenticationEndpoints
+{
+    public static void MapAuthenticationEndpoints(this IEndpointRouteBuilder routeBuilder)
+    {
+        routeBuilder.MapPost("/api/login", async (LoginDto loginDto, IAuthManager authManager) =>
+        {
+            var response = await authManager.Login(loginDto);
+
+            if (response is null)
+            {
+                return Results.Unauthorized();
+            }
+
+            return Results.Ok(response);
+
+        })
+        .WithTags("Authentication")
+        .WithName("Login")
+        .Produces(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status401Unauthorized);
+    }
+}
