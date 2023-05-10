84078fe7803ae24e5adf4cb5cf0662688ebbe498
Kevin Morgan <kmorgan26@gmail.com>
2023-05-10 04:48:55 -0500

    Add List of LotDto to PlatformDto

--- WebApiTraining/DTOs/Platform/PlatformDetailsDto.cs
+++ WebApiTraining/DTOs/Platform/PlatformDetailsDto.cs
@@ -1,7 +1,9 @@
-﻿using WebApiTraining.DTOs.Simulator;
+﻿using WebApiTraining.DTOs.Lot;
+using WebApiTraining.DTOs.Simulator;
 
 namespace WebApiTraining.DTOs.Platform;
 public class PlatformDetailsDto : PlatformDto
 {
-    public List<SimulatorDto> Simulators { get; set; } = new List<SimulatorDto>();
+    public List<SimulatorDto>? Simulators { get; set; }
+    public List<LotDto>? Lots { get; set; }
 }
