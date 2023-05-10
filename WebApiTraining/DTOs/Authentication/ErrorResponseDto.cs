ac9cfd4715a5dc380faa16e133760e7fd737a294
Kevin D. Morgan <kevin.d.morgan@gdit.com>
2023-05-10 09:54:45 -0500

    Add DTO for ErrorResponseDto

---
+++ WebApiTraining/DTOs/Authentication/ErrorResponseDto.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApiTraining.DTOs.Authentication;
+
+public class ErrorResponseDto
+{
+    public string Code { get; set; } = null!;
+    public string Description { get; set; } = null!;
+}
