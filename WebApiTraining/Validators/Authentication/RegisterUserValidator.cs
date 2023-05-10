125d39e44d0e01cbcaa21925b76da549aa992ee7
Kevin D. Morgan <kevin.d.morgan@gdit.com>
2023-05-10 12:25:21 -0500

    Add validator for Registration

---
+++ WebApiTraining/Validators/Authentication/RegisterUserValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using WebApiTraining.DTOs.Authentication;
+
+namespace WebApiTraining.Validators.Authentication;
+
+public class RegisterUserValidator : AbstractValidator<RegisterUserDto>
+{
+    public RegisterUserValidator()
+    {
+        RuleFor(i => i.EmailAddress)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(i => i.Password)
+            .MinimumLength(12)
+            .MaximumLength(20);
+
+        RuleFor(i => i.FirstName)
+            .NotEmpty();
+        RuleFor(i => i.LastName)
+            .NotEmpty();
+    }
+}
