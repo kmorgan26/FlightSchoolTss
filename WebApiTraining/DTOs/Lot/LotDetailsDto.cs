2a8b4d7e877dc39ff19f7bde49145a4ae6520f2a
Kevin Morgan <kmorgan26@gmail.com>
2023-05-10 04:48:04 -0500

    Create LotDto and LotDetailsDto

---
+++ WebApiTraining/DTOs/Lot/LotDetailsDto.cs
@@ -0,0 +1,8 @@
+﻿using WebApiTraining.DTOs.ManModule;
+
+namespace WebApiTraining.DTOs.Lot;
+
+public class LotDetailsDto : LotDto
+{
+    public virtual ICollection<ManModuleDto> ManModules { get; set; } = null!;
+}
--- WebApiTraining/DTOs/Lot/LotDto.cs
+++ WebApiTraining/DTOs/Lot/LotDto.cs
@@ -1,11 +1,7 @@
-﻿using WebApiTraining.DTOs.Platform;
-
-namespace WebApiTraining.DTOs.Lot;
+﻿namespace WebApiTraining.DTOs.Lot;
 public class LotDto
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = null!;
     public int PlatformId { get; set; }
-
-    public virtual PlatformDto Platform { get; set; }
 }
