1db306cc8bb1ba89ddd58353ab29f45fc0eb8b99
Kevin D. Morgan <kevin.d.morgan@gdit.com>
2023-05-24 09:56:00 -0500

    Add Repo for Maintainable

---
+++ FlightSchoolTss.Data/Interfaces/IMaintainableRepository.cs
@@ -0,0 +1,9 @@
+﻿using FlightSchoolTss.Data.Entities;
+using FlightSchoolTss.Data.Interfaces.Generic;
+
+namespace FlightSchoolTss.Data.Interfaces;
+
+public interface IMaintainableRepository : IGenericRepository<Maintainable>
+{
+
+}
---
+++ FlightSchoolTss.Data/Repositories/MaintainableRepository.cs
@@ -0,0 +1,10 @@
+﻿using FlightSchoolTss.Data.Data;
+using FlightSchoolTss.Data.Entities;
+using FlightSchoolTss.Data.Interfaces;
+
+namespace FlightSchoolTss.Data.Repositories;
+
+public class MaintainableRepository : GenericRepository<Maintainable>, IMaintainableRepository
+{
+    public MaintainableRepository(FstssDataContext context): base(context) { }
+}
