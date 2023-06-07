c2c999444f7a51d79102ecdc171873fabe898102
Kevin Morgan <kmorgan26@gmail.com>
2023-06-07 16:15:13 -0500

    Add function to Validate

--- FlightSchoolTss.Data/Validators/Maintainer/CreateMaintainerDtoValidator.cs
+++ FlightSchoolTss.Data/Validators/Maintainer/CreateMaintainerDtoValidator.cs
@@ -1,7 +1,7 @@
 ﻿using FluentValidation;
 using FlightSchoolTss.DTOs.Maintainer;
 
-namespace FlightSchoolTss.Validators.Lot;
+namespace FlightSchoolTss.Validators.Maintainer;
 
 public class CreateMaintainerDtoValidator : AbstractValidator<CreateMaintainerDto>
 {
@@ -13,4 +13,12 @@ public class CreateMaintainerDtoValidator : AbstractValidator<CreateMaintainerDt
             .MinimumLength(5).WithMessage("Please use at least 5 characters for the name")
             .MaximumLength(50).WithMessage("The name cannot be more than 50 characters");
     }
+
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+    {
+        var result = await ValidateAsync(ValidationContext<CreateMaintainerDto>.CreateWithOptions((CreateMaintainerDto)model, x => x.IncludeProperties(propertyName)));
+        if (result.IsValid)
+            return Array.Empty<string>();
+        return result.Errors.Select(e => e.ErrorMessage);
+    };
 }
