f24f3e508e29fc3b9071192e0a787ff66a60712f
Kevin D. Morgan <kevin.d.morgan@gdit.com>
2023-06-13 08:50:22 -0500

    Add validator for MaintainerDto

---
+++ FlightSchoolTss.Data/Validators/Maintainer/MaintainerDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using FlightSchoolTss.DTOs.Maintainer;
+
+namespace FlightSchoolTss.Validators.Maintainer;
+
+public class MaintainerDtoValidator : AbstractValidator<MaintainerDto>
+{
+    public MaintainerDtoValidator()
+    {
+        RuleFor(i => i.Name)
+            .NotEmpty()
+            .NotNull()
+            .MinimumLength(3).WithMessage("Please use at least 3 characters for the name")
+            .MaximumLength(50).WithMessage("The name cannot be more than 50 characters");
+    }
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+    {
+        var result = await ValidateAsync(ValidationContext<MaintainerDto>.CreateWithOptions((MaintainerDto)model, x => x.IncludeProperties(propertyName)));
+        if (result.IsValid)
+            return Array.Empty<string>();
+        return result.Errors.Select(e => e.ErrorMessage);
+    };
+}
