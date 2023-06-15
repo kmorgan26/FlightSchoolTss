3c31741afe1cd437617e28195da53c9d04c0f8b2
Kevin D. Morgan <kevin.d.morgan@gdit.com>
2023-06-15 13:45:10 -0500

    Implement MaintainerMode Feature

---
+++ FlightSchoolCm.UI/Features/MaintainerMode/Store/MaintainerModeStore.cs
@@ -0,0 +1,36 @@
+﻿using Fluxor;
+
+namespace FlightSchoolCm.UI.Features.MaintainerMode.Store;
+
+public record MaintainerModeState
+{
+    public int MaintainerModeId { get; init; }
+
+}
+
+public class MaintainerModeFeature : Feature<MaintainerModeState>
+{
+    public override string GetName() => "MaintainerMode";
+
+    protected override MaintainerModeState GetInitialState()
+    {
+        return new MaintainerModeState
+        {
+            MaintainerModeId = 0
+        };
+    }
+}
+
+public class MaintainerModeChangeAction { }
+
+public static class MaintainerModeReducers
+{
+    [ReducerMethod(typeof(MaintainerModeChangeAction))]
+    public static MaintainerModeState OnChange(MaintainerModeState state)
+    {
+        return state with
+        {
+            MaintainerModeId = state.MaintainerModeId + 1
+        };
+    }
+}
