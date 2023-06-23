616211f81182d35c0e1bfc662764ce09191224b6
Kevin Morgan <kmorgan26@gmail.com>
2023-06-23 08:06:17 -0500

    Work on FormComponent

---
+++ FlightSchoolCm.UI/Models/FstssFormOptions.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using MudBlazor;
+
+namespace FlightSchoolCm.UI.Models;
+
+public class FstssFormOptions<TItem> where TItem : class
+{
+    public string ButtonText { get; set; } = "ADD";
+    
+    public Variant? ButtonVariant { get; set; } = Variant.Filled;
+
+    public Color? ButtonColor { get; set; } = Color.Primary;
+
+    public Size? ButtonSize { get; set; } = Size.Medium;
+
+    public TItem? FormModel { get; set; }
+
+    public List<MudTextField<TItem>>? Fields { get; set; }
+
+    public MudForm Form { get; set; } = null!;
+
+    public AbstractValidator<TItem> Validator { get; set; } = null!;
+
+    public Func<MudForm>? FormSubmit { get; set; }
+}
