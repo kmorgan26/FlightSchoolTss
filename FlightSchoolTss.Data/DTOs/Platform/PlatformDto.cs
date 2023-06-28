3d65c342eea0f06a20d40be888dbdcc3440dbc0b
Kevin Morgan <kmorgan26@gmail.com>
2023-06-28 13:39:21 -0500

    Use FSForm for Platform.

--- FlightSchoolTss.Data/DTOs/Platform/PlatformDto.cs
+++ FlightSchoolTss.Data/DTOs/Platform/PlatformDto.cs
@@ -1,11 +1,18 @@
-﻿using FlightSchoolTss.Data.DTOs.Maintainer;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace FlightSchoolTss.Data.DTOs.Platform;
 public class PlatformDto
 {
     public int Id { get; set; } = 0;
+
+    [Required,
+        MaxLength(50, ErrorMessage = "The name cannot be more than 50 characters"),
+        MinLength(3, ErrorMessage = "Please use at least 3 characters for the name")]
     public string Name { get; set; } = null!;
     public bool IsActive { get; set; }
+
+    [Required(ErrorMessage = "Please select a maintainer"),
+        Range(0,int.MaxValue, ErrorMessage ="Please Select a Maintainer")]
     public int MaintainerId { get; set; } = 0;
     public int MaintainableId { get; set; } = 0;
 }
