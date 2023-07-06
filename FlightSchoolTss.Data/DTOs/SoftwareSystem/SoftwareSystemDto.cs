42f407c203483188de0a7901cd8f8b2bb88d2d5b
Kevin D. Morgan <kevin.d.morgan@gdit.com>
2023-07-06 14:27:20 -0500

    Add data annotations

--- FlightSchoolTss.Data/DTOs/SoftwareSystem/SoftwareSystemDto.cs
+++ FlightSchoolTss.Data/DTOs/SoftwareSystem/SoftwareSystemDto.cs
@@ -1,8 +1,14 @@
-﻿namespace FlightSchoolTss.Data.DTOs.SoftwareSystem;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightSchoolTss.Data.DTOs.SoftwareSystem;
 
 public class SoftwareSystemDto
 {
     public int Id { get; set; }
+
+    [Required]
+    [MaxLength(50, ErrorMessage = "The name cannot be more than 50 characters")]
+    [MinLength(3, ErrorMessage = "Please use at least 3 characters for the name")]
     public string Name { get; set; } = null!;
     public int MaintainableId { get; set; }
 
