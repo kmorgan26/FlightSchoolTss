e99826c81264907b4bedacb35a22c99fe0b71a89
Kevin D. Morgan <kevin.d.morgan@gdit.com>
2023-07-11 13:24:41 -0500

    Add methods

--- FlightSchoolCm.UI/Interfaces/IPlatformApiClient.cs
+++ FlightSchoolCm.UI/Interfaces/IPlatformApiClient.cs
@@ -1,4 +1,5 @@
-﻿using FlightSchoolTss.Data.DTOs.Platform;
+﻿using FlightSchoolTss.Data.DTOs.Lot;
+using FlightSchoolTss.Data.DTOs.Platform;
 using FlightSchoolTss.Data.DTOs.Simulator;
 
 namespace FlightSchoolCm.UI.Interfaces;
@@ -7,5 +8,7 @@ public interface IPlatformApiClient
 {
     Task<List<PlatformDetailsDto>> GetPlatformDetailsAsync();
     Task<IEnumerable<PlatformDto>> GetPlatformDtosByMaintainerIdAsync(int id);
+    Task<IEnumerable<SimulatorDto>> GetSimulatorDtosByPlatformIdAsync(int id);
+    Task<IEnumerable<LotDto>> GetLotDtosAsync();
     Task<List<SimulatorDto>> GetSimulatorDtosAsync();
 }
