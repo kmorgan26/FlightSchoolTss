51720de0ef9c01ec0a16a52b498837e038c3ba97
Kevin D. Morgan <kevin.d.morgan@gdit.com>
2023-07-12 10:15:48 -0500

    Add method GetManModuldsByLotIdAsync

--- FlightSchoolTss.Data/Interfaces/IManModuleRepository.cs
+++ FlightSchoolTss.Data/Interfaces/IManModuleRepository.cs
@@ -2,4 +2,7 @@
 using FlightSchoolTss.Data.Interfaces.Generic;
 
 namespace FlightSchoolTss.Data.Interfaces;
-public interface IManModuleRepository : IGenericRepository<ManModule>{}
+public interface IManModuleRepository : IGenericRepository<ManModule>
+{
+    Task<IEnumerable<ManModule>> GetManModulesByLotIdAsync(int id);
+}
--- FlightSchoolTss.Data/Repositories/ManModuleRepository.cs
+++ FlightSchoolTss.Data/Repositories/ManModuleRepository.cs
@@ -1,11 +1,20 @@
 ﻿using FlightSchoolTss.Data.Data;
 using FlightSchoolTss.Data.Entities;
 using FlightSchoolTss.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace FlightSchoolTss.Data.Repositories;
 public class ManModuleRepository : GenericRepository<ManModule>, IManModuleRepository
 {
     public ManModuleRepository(FstssDataContext context) : base(context)
     {
+    }
+
+    public async Task<IEnumerable<ManModule>> GetManModulesByLotIdAsync(int id)
+    {
+        return await _context.ManModules
+            .Where(s => s.LotId == id)
+            .OrderBy(s => s.Name)
+            .ToListAsync();
     }
 }
